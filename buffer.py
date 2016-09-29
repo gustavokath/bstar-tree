93ba455004bd76a32ba10e4ea95538089527d924
Pedro Vanzella <pedro@pedrovanzella.com>
2016-09-29 19:27:11 -0300

    Write back cache

--- buffer.py
+++ buffer.py
@@ -17,5 +17,28 @@ class Buffer:
                 return self._buff[i]
 
         # if not, try to write it to the cache
+        dblock = self.datafile.get_datablock(address)
+        self._write_to_buffer(dblock)
+        return dblock
+
+    def _write_to_buffer(self, dblock):
         # find the next empty cell
-        # if none is found, write over the oldest one
+        if self._last < BUFFER_SIZE:  # there's room in the buffer
+            self._buff[self._last] = dblock
+            self._last += 1
+        else:  # if none is found, write over the oldest one
+            # check if we need to write back
+            old = self._buff[self._next]
+            # if _next is dirty
+            if old.dirty:
+                # write back
+                self.datafile.write_datablock(old)
+
+            # write to the first in
+            self._buff[self._next] = dblock
+
+            # set the pointer to the next oldest
+            if self._next >= BUFFER_SIZE - 1:
+                self._next = 0
+            else:
+                self._next += 1
