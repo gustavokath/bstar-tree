f8f19b47cf987ee63a6dd19351d2d8c19c6480fe
Pedro Vanzella <pedro@pedrovanzella.com>
2016-09-29 19:35:46 -0300

    Mark Datablock as dirty when we change it

--- datablock.py
+++ datablock.py
@@ -8,6 +8,10 @@ class Datablock:
     dirty = attr.ib(default=False)
     DATABLOCK_SIZE = 2 * 1024 * 8
 
+    def set_data(self, data):
+        self.data = data
+        self.dirty = True
+
     @classmethod
     def from_bytes(cls, address, data):
         """
